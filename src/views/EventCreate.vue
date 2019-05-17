7b06b048245ea0f4e887f7e3f01f29199a67844d
Ndéné SENE <ndenenafi10@gmail.com>
2019-05-17 11:23:13 +0000

    First Commit

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="event-create">
+    <h1>This is an event create page</h1>
+  </div>
+</template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js'
+
+import { log } from 'util';
+    export default {
+        components: {
+            EventCard
+        },
+        data() {
+            return {
+                events: []
+            }
+        },
+        created () {
+            EventService.getEvents()
+                .then(response => {
+                    console.log(response.data);
+                    this.events = response.data
+                })
+                .catch(error => {
+                    console.log(error.response);
+                })
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
